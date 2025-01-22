6795c70541210d8ac51e71e7b4eb9072c84201d7
pmboutet <37446103+pmboutet@users.noreply.github.com>
2025-01-22 22:05:59 +0100

    Update URLs to include download endpoints

--- fundraising/urls.py
+++ fundraising/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.configuration import ConfigurationViewSet
+from .views.download import DownloadDatasetView, DatasetStatusView
 from .views import GenerateDatasetView
 
 router = DefaultRouter()
@@ -11,4 +12,6 @@ app_name = 'fundraising'
 urlpatterns = [
     path('', include(router.urls)),
     path('generate/', GenerateDatasetView.as_view(), name='generate_dataset'),
+    path('datasets/<int:dataset_id>/download/', DownloadDatasetView.as_view(), name='download_dataset'),
+    path('datasets/<int:dataset_id>/status/', DatasetStatusView.as_view(), name='dataset_status'),
 ]
