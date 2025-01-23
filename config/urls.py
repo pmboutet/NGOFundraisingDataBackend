26e26d74ab1565956f0eda7bbc1eacf0c01488f7
pmboutet <37446103+pmboutet@users.noreply.github.com>
2025-01-23 15:00:55 +0100

    Add root URL redirect to API documentation

--- config/urls.py
+++ config/urls.py
@@ -1,24 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-    SpectacularRedocView,
-)
+from django.views.generic import RedirectView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/docs/', permanent=True)),
     path('admin/', admin.site.urls),
     path('api/', include('fundraising.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    
-    # API Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
