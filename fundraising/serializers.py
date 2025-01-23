2db2ef3c3ab1ba1c67cc98c2d76ca68e7c154eb4
pmboutet <37446103+pmboutet@users.noreply.github.com>
2025-01-23 10:02:03 +0100

    Add views and serializers

--- fundraising/serializers.py
+++ fundraising/serializers.py
@@ -1,16 +1,7 @@
 from rest_framework import serializers
 from .models import DatasetConfiguration, GeneratedDataset
 
-class GeneratedDatasetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GeneratedDataset
-        fields = ['id', 'transactions_file', 'contacts_file', 'generated_at', 'download_count']
-        read_only_fields = ['id', 'generated_at', 'download_count']
-
 class DatasetConfigurationSerializer(serializers.ModelSerializer):
-    generated_datasets = GeneratedDatasetSerializer(many=True, read_only=True)
-    
     class Meta:
         model = DatasetConfiguration
-        fields = ['id', 'name', 'config_file', 'created_at', 'status', 'error_message', 'generated_datasets']
-        read_only_fields = ['id', 'created_at', 'status', 'error_message']
+        fields = '__all__'
